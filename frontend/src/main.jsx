0da3484ca08b82397e2d2d1e8be1d0f6ab528056
Saravanan2006 <saravanasaravanan4179@gamil.com>
2025-07-19 23:07:42 +0530

    Commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { UserContextProvider } from './context/UserContext.jsx'
 import { CourseContextProvider } from './context/CourseContext.jsx';
-``
-export const server = 'https://e-learning-platform-7jc2.onrender.com';
+
+export const server = 'https://e-learning-platform-i2og.onrender.com';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
